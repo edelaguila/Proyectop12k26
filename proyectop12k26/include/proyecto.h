ed7362dcc30f218cde719581ecbe24cb8f3785e3
Esduardo Del Aguila <daguilae@yahoo.com>
2026-04-22 05:21:06 -0600

    inicio proyecto final

---
+++ proyectop12k26/include/proyecto.h
@@ -0,0 +1,16 @@
+#ifndef PROYECTO_H
+#define PROYECTO_H
+
+
+class proyecto
+{
+    public:
+        proyecto();
+        virtual ~proyecto();
+
+    protected:
+
+    private:
+};
+
+#endif // PROYECTO_H
---
+++ proyectop12k26/src/proyecto.cpp
@@ -0,0 +1,11 @@
+#include "proyecto.h"
+
+proyecto::proyecto()
+{
+    //ctor
+}
+
+proyecto::~proyecto()
+{
+    //dtor
+}
