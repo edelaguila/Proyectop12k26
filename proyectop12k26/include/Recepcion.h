58300dde98c0f654619c6a5092337953f22923a7
damadop-commits <damadop@miumg.edu.gt>
2026-05-01 19:48:37 -0600

    Modulo Recepcion de Pagos + main provisional

---
+++ proyectop12k26/include/Recepcion.h
@@ -0,0 +1,21 @@
+#ifndef RECEPCION_H
+#define RECEPCION_H
+
+#include <iostream>
+#include <string>
+#include "Alumnos.h"
+
+using namespace std;
+
+class Recepcion
+{
+    public:
+
+        void validarAlumno(Alumnos alumno);
+
+
+        void validarDocente();
+        void validarEmpleado();
+};
+
+#endif
